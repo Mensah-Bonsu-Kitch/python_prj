7d353e2fce27a1961cadd2be8e7e42e1b3fc2346
Kitch Mensah Bonsu <mensahbonsukitch@gmail.com>
2022-06-06 21:22:57 -0400

    Amplitude modulation

---
+++ Amplitude_modulation.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi as p
+
+Ac = eval(input('Enter carrier Amplitude: '))
+Am = eval(input('Enter the message signal amplitude: '))
+fc = eval(input('Enter carrier signal frequency: '))
+fm = eval(input('Enter message signal frequency: '))
+
+t = np.arange(-0.25,0.25,0.0001)
+mu = Am/Ac
+s = Ac*(1+mu*np.cos(2*p*fm*t))*np.cos(2*p*fc*t)
+
+plt.plot(t,s,'r')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.title('Amplitude modulated signal')
+
+plt.show()
+plt.grid()
